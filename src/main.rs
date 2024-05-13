417599af4d7ed4335a71a04bb01342ca43271faa
Sean Merz <smmerz56@yahoo.com>
2024-05-13 15:56:18 -0700

    final version

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,49 @@
+use std::{env, fs};
 use colored::*;
 use regex::Regex;
-use std::env;
-use std::fs;
+
 
 #[derive(Debug)]
-struct Arguments<'a> {
+struct Arguements<'a> {
     pattern: &'a String,
-    replace: &'a String,
     input_file: &'a String,
     output_file: &'a String,
+    replace: &'a String,
 }
 
-fn parse_args(argc: usize, argv: &Vec<String>) -> Arguments {
+fn main(){
+
+    let argv: Vec<String> = env::args().collect();
+    let argc = argv.len();
+
+    run(argc, argv)
+}
+
+fn run(argc: usize, argv: Vec<String>){
+    let args = parse_args(argc, &argv);
+    let data = read_file_content(&args.input_file);
+
+    let replaced_data = match replace(&args.pattern, &args.replace, &data) {
+        Ok(data)=>data,
+        Err(_) => {
+            eprintln!("{} Failed to replace data!", "Error".red());
+            std::process::exit(1);
+        }
+    };
+
+    write(&args.output_file, &replaced_data);
+}
+
+fn parse_args(argc: usize, argv: &Vec<String>) -> Arguements{
+    println!("{}",argc);//Simulating logging
+    println!("{}",argv.len());
+
     if argc != 5 {
-        eprintln!("{} wrong number of arguments!", "ERROR".red());
-        std::process::exit(1);
+        eprintln!("{} Wrong number of arguements!","ERROR".red());
+        std::process::exit(400);
     }
 
-    Arguments {
+    return  Arguements{
         pattern: &argv[1],
         replace: &argv[2],
         input_file: &argv[3],
@@ -25,25 +51,11 @@ fn parse_args(argc: usize, argv: &Vec<String>) -> Arguments {
     }
 }
 
-fn read(filepath: &str) -> String {
-    match fs::read_to_string(filepath) {
+fn read_file_content(file_path: &str) -> String{
+    match fs::read_to_string(&file_path) {
         Ok(data) => data,
         Err(_) => {
-            eprintln!("{} Failed to read from file {}!", "ERROR".red(), &filepath);
-            std::process::exit(1);
-        }
-    }
-}
-
-fn write(output_file: &str, data: &str) {
-    match fs::write(&output_file, &data) {
-        Ok(_) => {}
-        Err(_) => {
-            eprintln!(
-                "{} Failed to write to file {}!",
-                "ERROR".red(),
-                &output_file
-            );
+            eprintln!("{} Failed to read from file {}", "Error".red(), &file_path);
             std::process::exit(1);
         }
     }
@@ -55,25 +67,12 @@ fn replace(search_pattern: &str, replacement: &str, data: &str) -> Result<String
     Ok(regex.replace_all(data, replacement).to_string())
 }
 
-fn run(argc: usize, argv: Vec<String>) {
-    let args = parse_args(argc, &argv);
-    let data = read(&args.input_file);
-
-    let replaced_data = match replace(&args.pattern, &args.replace, &data) {
-        Ok(data) => data,
+fn write(output_file: &str, data: &str){
+    match fs::write(&output_file, &data) {
+        Ok(_) => {}
         Err(_) => {
-            eprintln!("{} Failed to replace data!", "ERROR".red(),);
+            eprintln!("{} Failed to write to file {}", "Error".red(), &output_file);
             std::process::exit(1);
         }
-    };
-
-    write(&args.output_file, &replaced_data);
-}
-
-fn main() {
-    
-    let argv: Vec<String> = env::args().collect();
-    let argc = argv.len();
-
-    run(argc, argv)
+    }
 }
